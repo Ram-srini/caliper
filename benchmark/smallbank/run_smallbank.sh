f64e5ef3f954181df11e84ca79a8241ef15ee2b7
Ramakrishna Srinivasamurthy <ramakrishna.srinivasamurthy@intel.com>
2018-09-03 02:53:09 -0700

    Added the scripts run the tests

---
+++ benchmark/smallbank/run_smallbank.sh
@@ -0,0 +1,5 @@
+cd /home/builder/fabric/caliper/benchmark/smallbank
+node main.js -c config_longrun.json -n fabric.json
+sleep 10
+node main.js -c config.json -n fabric.json
+ps -ef |grep dstat |grep -v grep | cut -d ' ' -f 3 |xargs kill -9
