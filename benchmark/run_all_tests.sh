72fe122f87e97525988b005798a7741f552d7544
Ramakrishna Srinivasamurthy <ramakrishna.srinivasamurthy@gmail.com>
2018-09-09 09:37:03 -0700

    Removed comment
    Signed-off-by: Ramakrishna Srinivasamurthy <ramakrishna.srinivasamurthy@gmail.com>

--- benchmark/run_all_tests.sh
+++ benchmark/run_all_tests.sh
@@ -1,8 +1,8 @@
 cd /home/builder/fabric/caliper/benchmark/smallbank
-#/opt/intel/storage-snapshot/sps-start.sh
-#node main.js -c config.json -n fabric.json
-#/opt/intel/storage-snapshot/sps-stop.sh
-#sleep 10
+/opt/intel/storage-snapshot/sps-start.sh
+node main.js -c config.json -n fabric.json
+/opt/intel/storage-snapshot/sps-stop.sh
+sleep 20
 /opt/intel/storage-snapshot/sps-start.sh
 node main.js -c config_longrun.json -n fabric.json
 /opt/intel/storage-snapshot/sps-stop.sh
