d9fe16be9d7d92ad973a35309cf8889d4a64fb43
builder <builder@server01.localdomain>
2018-10-08 22:23:39 -0700

    changed test run scripts with right order

--- benchmark/run_all_tests.sh
+++ benchmark/run_all_tests.sh
@@ -1,16 +1,18 @@
-cd /home/builder/fabric/caliper/benchmark/smallbank
+cd ./simple
 /opt/intel/storage-snapshot/sps-start.sh
 node main.js -c config.json -n fabric.json
 /opt/intel/storage-snapshot/sps-stop.sh
 sleep 20
+cd ./smallbank
+/opt/intel/storage-snapshot/sps-start.sh
+node main.js -c config.json -n fabric.json
+/opt/intel/storage-snapshot/sps-stop.sh
+sleep 20
+cd ./simple
 /opt/intel/storage-snapshot/sps-start.sh
 node main.js -c config_longrun.json -n fabric.json
 /opt/intel/storage-snapshot/sps-stop.sh
-sleep 20
-cd /home/builder/fabric/caliper/benchmark/simple
-/opt/intel/storage-snapshot/sps-start.sh
-node main.js -c config.json -n fabric.json
-/opt/intel/storage-snapshot/sps-stop.sh
+cd ./smallbank
 sleep 20
 /opt/intel/storage-snapshot/sps-start.sh
 node main.js -c config_longrun.json -n fabric.json
