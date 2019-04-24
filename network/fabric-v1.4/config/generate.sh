94c55c43c55e3471e6ff9232e2d8d19b110bd255
Ramakrishna Srinivasamurthy <ramakrishna.srinivasamurthy@intel.com>
2019-04-24 02:53:26 -0700

    Added 8 peers for solo consensus
    Signed-off-by: Ramakrishna Srinivasamurthy <ramakrishna.srinivasamurthy@intel.com>

--- network/fabric-v1.4/config/generate.sh
+++ network/fabric-v1.4/config/generate.sh
@@ -4,9 +4,9 @@
 # export FABRIC_CFG_PATH=<path to directory>
 
 # The below assumes you have the relevant code available to generate the cryto-material
-/home/fabric/fabric-samples/bin/cryptogen generate --config=./crypto-config.yaml
-/home/fabric/fabric-samples/bin/configtxgen -profile OrgsOrdererGenesis -outputBlock orgs.genesis.block -channelID mychannel
-/home/fabric/fabric-samples/bin/configtxgen -profile OrgsChannel -outputCreateChannelTx mychannel.tx -channelID mychannel
+~/fabric-samples/bin/cryptogen generate --config=./crypto-config.yaml
+~/fabric-samples/bin/configtxgen -profile OrgsOrdererGenesis -outputBlock orgs.genesis.block 
+~/fabric-samples/bin/configtxgen -profile OrgsChannel -outputCreateChannelTx mychannel.tx -channelID mychannel
 
 # Rename the key files we use to be key.pem instead of a uuid
 for KEY in $(find crypto-config -type f -name "*_sk"); do
