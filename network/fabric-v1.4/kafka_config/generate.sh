421d9fb9c2bac6a377a4463dd50ee351d59fdc79
Ramakrishna Srinivasamurthy <ramakrishna.srinivasamurthy@intel.com>
2019-04-23 23:11:39 -0700

    Added artificacts for 8 peers and 4 orderers
    Signed-off-by: Ramakrishna Srinivasamurthy <ramakrishna.srinivasamurthy@intel.com>

--- network/fabric-v1.4/kafka_config/generate.sh
+++ network/fabric-v1.4/kafka_config/generate.sh
@@ -4,11 +4,13 @@
 # export FABRIC_CFG_PATH=<path to directory>
 
 # The below assumes you have the relevant code available to generate the cryto-material
-export FABRIC_SAMPLE_BIN=~/fabric-samples-1.4/bin
-$FABRIC_SAMPLE_BIN/cryptogen generate --config=./crypto-config.yaml
-$FABRIC_SAMPLE_BIN/configtxgen -profile OrgsOrdererGenesis -outputBlock orgs.genesis.block
-$FABRIC_SAMPLE_BIN/configtxgen -profile OrgsChannel1 -outputCreateChannelTx mychannel.tx -channelID mychannel
-$FABRIC_SAMPLE_BIN/configtxgen -profile OrgsChannel2 -outputCreateChannelTx yourchannel.tx -channelID yourchannel
+~/fabric-samples/bin/cryptogen generate --config=./crypto-config.yaml
+~/fabric-samples/bin/configtxgen -profile OrgsOrdererGenesis -outputBlock orgs.genesis.block
+~/fabric-samples/bin/configtxgen -profile OrgsChannel1 -outputCreateChannelTx mychannel.tx -channelID mychannel
+~/fabric-samples/bin/configtxgen -profile OrgsChannel1 -outputCreateChannelTx mychannel1.tx -channelID mychannel1
+~/fabric-samples/bin/configtxgen -profile OrgsChannel2 -outputCreateChannelTx mychannel2.tx -channelID mychannel2
+~/fabric-samples/bin/configtxgen -profile OrgsChannel3 -outputCreateChannelTx mychannel3.tx -channelID mychannel3
+~/fabric-samples/bin/configtxgen -profile OrgsChannel4 -outputCreateChannelTx mychannel4.tx -channelID mychannel4
 
 # Rename the key files we use to be key.pem instead of a uuid
 for KEY in $(find crypto-config -type f -name "*_sk"); do
